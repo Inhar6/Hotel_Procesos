d2b16b8d3788371416fc7fdc29e0625412920ae7
Inhar Serrano <inhar.serrano@opendeusto.es>
2025-04-15 19:46:51 +0200

    Nueva anotación en HabitacionControllerTest

--- src/test/java/com/example/restapi/integracion/controller/HabitacionControllerIntegrationTest.java
+++ src/test/java/com/example/restapi/integracion/controller/HabitacionControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class HabitacionControllerIntegrationTest {
 
     @Autowired
@@ -209,5 +211,5 @@ public class HabitacionControllerIntegrationTest {
         
         assertEquals(total, ocupadas + disponibles);
     }
-         */
+        */  
 }
